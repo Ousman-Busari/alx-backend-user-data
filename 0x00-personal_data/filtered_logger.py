8087d8053cab54c7b5e4d9ce65d95370efde1e8e
Ousman <usman.traid@gmail.com>
2023-05-29 00:26:02 -0400

    parameter typo fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,13 +16,13 @@ PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, seperator: str) -> str:
+                 message: str, separator: str) -> str:
     """
     returns the log message obfuscated, and joined with seperator
     """
     for field in fields:
-        message = re.sub(field + "=.*?" + seperator,
-                         field + "=" + redaction + seperator, message)
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
     return message
 
 
